7bdf9a085074a561047a1f2c0765293072158766
aledwassell <aledwassell@googlemail.com>
2018-08-10 07:32:12 +0100

    finishing enemy ship

---
+++ Assets/EvasiveManeuver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundry
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class EvasiveManeuver : MonoBehaviour {
+
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+
+    private float currentSpeed;
+    public float dodge;
+    public float tilt;
+    public Boundry boundry;
+
+    private float targetManeuver;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(Evade());
+        currentSpeed = rb.velocity.z;
+	}
+
+    IEnumerator Evade(){
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while(true){
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign (transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime);
+        rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax),
+                0.0f,
+                Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax)
+            );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+}
